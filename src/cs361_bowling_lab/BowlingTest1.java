88a27c1c1efcb9afccc5fdcc5286dff391001cef
Michael Davis <Michael Davis@Michael-Laptop.mshome.net>
2018-01-30 14:21:35 -0600

    added test cases

---
+++ src/cs361_bowling_lab/BowlingTest1.java
@@ -0,0 +1,37 @@
+package cs361_bowling_lab;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.internal.runners.JUnit38ClassRunner;
+
+public class BowlingTest1 {
+	TestSuite testSuite1 = new TestSuite();
+	TestSuite testSuite2 = new TestSuite();
+	TestSuite testSuite3 = new TestSuite();
+	TestSuite testSuite4 = new TestSuite();
+	
+	
+	
+	@Test
+	public void testOneThrow() {
+		
+	}
+	
+	@Test
+	public void testTwoThrows() {
+		
+	}
+	
+	@Test
+	public void testThreeThrows() {
+		
+	}
+	
+	@Test
+	public void testThrowOn11thFrame() {
+		
+	}
+
+	
+
+}
---
+++ src/cs361_bowling_lab/TestSuite.java
@@ -0,0 +1,11 @@
+package cs361_bowling_lab;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ 
+	BowlingTest1.class, 
+	BowlingTest2.class})
+
+public class TestSuite {
+}
